dc9a74dae99f43889558695e3b801fc4b5671d32
Qwikle <qwikle.ziko@gmail.com>
2023-04-13 20:09:06 +0200

    docs: :memo: add documentation for route

--- front/src/routes/route.js
+++ front/src/routes/route.js
@@ -1,4 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
+/**
+ * La constante routes contient les routes de l'application
+ * Le path est l'url de la page
+ * Le name est le nom de la page
+ * Le component est le composant qui sera chargé
+ */
 export const routes = [
   {
     path: "/",
@@ -7,6 +13,13 @@ export const routes = [
   },
 ];
 
+/**
+ * La constante router est le router de l'application
+ * createWebHistory() permet de créer un historique de navigation
+ * en d'autres termes, quand on clique sur le bouton retour du navigateur
+ * on revient à la page précédente
+ * routes est la constante qui contient les routes de l'application
+ */
 const router = createRouter({
   history: createWebHistory(),
   routes,
