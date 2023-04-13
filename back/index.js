8075100802e90933e85f930f978c527f1cd13bc4
Elyette <elyette.gandji@gmail.com>
2023-04-13 08:55:50 +0200

    npm uninstall express-session

--- back/index.js
+++ back/index.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const expressValidator = require('express-validator');
-const session = require('express-session');
 const router = require('./app/routers');
 const middlewares = require('./app/middlewares');
 
@@ -16,15 +15,6 @@ app.use(cors('*'));
 // On demande à Express d'extraire les données des requêtes POST
 app.use(express.urlencoded({ extended: true })); // Pour parser les body de type `x-www-form-url-encoded` et les ajouter au req.body
 app.use(express.json()); // Pour parser les body de type JSON
-
-// Charger les données de la session sur `req.session` et `res.locals`
-app.use(
-  session({
-      saveUninitialized: true,
-      resave: true,
-      secret: 'Un secret pour signer les id de sessions',
-  })
-);
 
 // On plug le router
 app.use(router);
