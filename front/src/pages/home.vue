d1913d5d0f61819c78c32e23de670e52bc680866
Qwikle <qwikle.ziko@gmail.com>
2023-04-14 11:33:49 +0200

    feat: :art: rename file and add seo meta

---
+++ front/src/pages/home.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { useSeoMeta } from "@unhead/vue";
+
+useSeoMeta({
+  title: "Bienvenue sur O'Commerce",
+  description: "Bienvenue sur O'Commerce",
+  keywords: "home, accueil",
+});
+</script>
+<template>
+  <div>
+    <h1>Page home</h1>
+  </div>
+</template>
