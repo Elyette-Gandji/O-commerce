a48e4301e0bd89ddf2047817c316651ba2f800c6
Qwikle <qwikle.ziko@gmail.com>
2023-04-14 11:53:37 +0200

    docs: :memo: add doc

--- front/src/pages/home.vue
+++ front/src/pages/home.vue
@@ -1,6 +1,10 @@
 <script setup>
 import { useSeoMeta } from "@unhead/vue";
-
+/**
+ * j'utilise la fonction useSeoMeta pour pouvoir utiliser le composant SeoMeta
+ * qui va me permettre de gérer les meta tags de ma page
+ * et me permettre d'optimiser le référencement du site
+ */
 useSeoMeta({
   title: "Bienvenue sur O'Commerce",
   description: "Bienvenue sur O'Commerce",
