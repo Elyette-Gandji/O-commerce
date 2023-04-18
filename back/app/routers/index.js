26fd1092323d5a120d607e07d48732ab23846aee
Elyette <elyette.gandji@gmail.com>
2023-04-18 10:22:24 +0200

    review productRouter.js et index.js dans router

--- back/app/routers/index.js
+++ back/app/routers/index.js
@@ -1,22 +1,14 @@
 // Créer un fichier de route distinct pour chaque contrôleur est une bonne pratique de développement pour mieux organiser le code et le rendre plus facile à maintenir.
 // Créer un fichier index.js routeurs permet de rassembler tous les fichiers de route en un seul et même endroit, ce qui facilite la gestion des routes de l'application.
 
+module.exports = router;
+
 const express = require("express");
 const router = express.Router();
 
-/*--------------- Controllers ---------------*/
-const productController = require("../controllers/productController");
-const categoryController = require('../controllers/categoryController');
+const productRouter = require("./productRouter");
 
-
-/*--------------- Routes category ---------------*/
-// Routes GET 
-router.get('/categories', categoryController.getAllCategory);
-router.get('/categories/:id', categoryController.getOneCategory);
-
-/*--------------- Routes product ---------------*/
-// Routes GET 
-router.get("/products", productController.getAllProducts)
-router.get("/product/:id", productController.getOneProduct);
+// Toutes les routes qui commencent par /products seront cherché dans le productRouter.
+router.use("/products", productRouter);
 
 module.exports = router;
--- back/app/routers/productRouter.js
+++ back/app/routers/productRouter.js
@@ -3,7 +3,7 @@ const productController = require("../controllers/productController");
 
 const router = express.Router();
 
-router.get("/products", productController.getAllProducts)
-router.get("/product/:id", productController.getOneProduct);
+router.get("/", productController.getAllProducts)
+router.get("/:id", productController.getOneProduct);
 
 module.exports = router;
